b57791a811dec6f9e3b8e563345fc3cbfcea20d9
Fox <declanschindler@gmail.com>
2024-06-25 12:53:45 -0400

    Added calling related tables

--- store/admin.py
+++ store/admin.py
@@ -4,7 +4,8 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection']
     list_editable = ['unit_price']
     list_per_page = 10
 
@@ -23,3 +24,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
